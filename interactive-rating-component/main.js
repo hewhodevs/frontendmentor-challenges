02f4e28040a6077c477f75788329053da7ccd9b5
hewhodevs <hewhodevs@outlook.com>
2022-08-11 17:02:49 +0930

    Show thankyou message on submit - added show and hide classes to style.css - submit onclick hides the form and shows the thankyou message - TBD: have the thankyou message show the rating selected by the user

---
+++ interactive-rating-component/main.js
@@ -0,0 +1,19 @@
+const submitButton = document.getElementById('rating__submit-button');
+const ratingFormContainer = document.getElementById('rating-form-container');
+const thankyouConfirmationContainer = document.getElementById('thankyou-confirmation-container');
+
+ratingFormContainer.onclick = () => {
+  hideRatingFormContainer();
+  showThankyouConfirmationContainer();
+  return false;
+}
+
+function hideRatingFormContainer() {
+  ratingFormContainer.classList.remove('show-rating-form');
+  ratingFormContainer.classList.add('hidden');
+}
+
+function showThankyouConfirmationContainer() {
+  thankyouConfirmationContainer.classList.remove('hidden');
+  thankyouConfirmationContainer.classList.add('show-thankyou-confirmation');
+}
