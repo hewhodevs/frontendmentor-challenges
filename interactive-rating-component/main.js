d538eaf55835d69fcf97e0c628f9747cc350e0fa
hewhodevs <hewhodevs@outlook.com>
2022-08-12 14:31:03 +0930

    Display submitted rating value in thankyou confirmation - onSubmit click, the thankyou message will now reflect the submitted rating checked

--- interactive-rating-component/main.js
+++ interactive-rating-component/main.js
@@ -1,19 +1,33 @@
-const submitButton = document.getElementById('rating__submit-button');
-const ratingFormContainer = document.getElementById('rating-form-container');
-const thankyouConfirmationContainer = document.getElementById('thankyou-confirmation-container');
-
-submitButton.onclick = () => {
+document.getElementById('rating__submit-button').onclick = (e) => {
+  e.preventDefault();
+  const ratingSubmitted = getUserSelectedRating();
   hideRatingFormContainer();
+  displayUserSelectedRating(ratingSubmitted);
   showThankyouConfirmationContainer();
-  return false;
 }
 
 function hideRatingFormContainer() {
+  const ratingFormContainer = document.getElementById('rating-form-container');
   ratingFormContainer.classList.remove('show-rating-form');
   ratingFormContainer.classList.add('hidden');
 }
 
 function showThankyouConfirmationContainer() {
+  const thankyouConfirmationContainer = document.getElementById('thankyou-confirmation-container');
   thankyouConfirmationContainer.classList.remove('hidden');
   thankyouConfirmationContainer.classList.add('show-thankyou-confirmation');
+}
+
+function getUserSelectedRating() {
+  const ratingsGroup = document.getElementsByName('ratings-group');
+  for (let i = 0; i < ratingsGroup.length; i++) {
+    const rating = ratingsGroup[i];
+    if (rating.checked) {
+      return rating.value;
+    }
+  }
+}
+
+function displayUserSelectedRating( rating ) {
+  document.getElementById('submitted-rating').textContent = rating;
 }
